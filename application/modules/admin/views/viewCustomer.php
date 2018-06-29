57c27750de8d66f6e5f5ff97f9cd549cfb2a2cc9
sushil <sushil@samosys.com>
2018-06-29 19:55:57 +0530

    service provider view, add, activate and deactivate, Improved design

---
+++ application/modules/admin/views/viewCustomer.php
@@ -0,0 +1,358 @@
+<div class="page page-tables-datatables">
+
+    <div class="pageheader">
+
+        <h2>View Customer <span></span></h2>
+    </div>
+
+    <!-- row -->
+    <div class="row">
+        <!-- col -->
+        <div class="col-md-12">
+
+            <section class="tile">
+
+                <!-- tile header -->
+                <div class="tile-header dvd dvd-btm">
+                    <h1 class="custom-font"><strong>View Customer  </strong></h1>
+                    <?php echo $this->session->flashdata('message'); ?>
+                </div>
+                <!-- /tile header -->
+
+                <!-- tile body -->
+                <div class="tile-body">
+                   <form action="<?php echo site_url();?>admin/add_servicePro" method='POST'>
+
+
+                    <?php  
+
+                    if(isset($fetch_customer)){
+
+
+
+                      foreach ($fetch_customer as $customer_data) {?>
+                     
+                      <div class="form-group">
+                        <label for="pwd">First Name:</label>
+                        <input type="text" readonly value="<?php echo $customer_data->firstname; ?>" class="form-control" id="firstname"  name="firstname">
+                      </div>
+
+                      <div class="form-group">
+                        <label for="pwd">Last Name:</label>
+                        <input type="text" value="<?php echo $customer_data->lastname; ?>" readonly  class="form-control" id="lastname"  name="lastname">
+                      </div>
+
+                      <div class="form-group">
+                        <label for="pwd">user Name:</label>
+                        <input type="text" value="<?php echo $customer_data->username; ?>" readonly class="form-control" id="username"  name="username">
+                      </div>
+
+                       <div class="form-group">
+                        <label for="pwd">Email:</label>
+                        <input type="text" value="<?php echo $customer_data->email; ?>" readonly class="form-control" id="email"  name="email">
+                      </div>
+
+                      
+
+                      <div class="form-group">
+                        <label for="pwd">Gender:</label>
+                        <select disabled  class="form-control" id="gender"  name="gender">
+                          
+                          <option value="1">
+
+                            <?php if($customer_data->gender = 1 ){ ?>
+                                    Male
+                            <?php } elseif($customer_data->gender = 0){?>
+
+                              Other
+                            <?php } elseif($customer_data->gender = 2){?>
+                                Female
+                              <?php } ?>
+                          </option>
+                          
+                        </select>
+                      </div>
+
+                      <div class="row">
+                          <div class="col-md-4">
+                            <div class="form-group">
+                              <label for="pwd">Country :</label>
+                              <select disabled class="form-control" id="country"  name="country">
+                               
+                                <?php if(isset($countries)){
+                                    foreach ($countries as  $countrie) { ?>
+                                <option value="<?php echo $countrie->id; ?>" <?php if($countrie->id == $customer_data->country_id){echo "selected";} ?>><?php echo $countrie->name; ?></option>
+                                <?php }} ?>
+                              </select>
+                            </div>
+                          </div>
+                          <div class="col-md-4">
+                            <div class="form-group">
+                              <label for="pwd">State:</label>
+                              <select  disabled class="form-control" id="state"  name="state">
+                                <?php if(isset($states)){
+                                    foreach ($states as  $state) { ?>
+                                <option value="<?php echo $state->id; ?>" <?php if($state->id == $customer_data->state_id){echo "selected";} ?>><?php echo $state->name; ?></option>
+                                <?php }} ?>
+                                
+                               
+                              </select>
+                            </div>
+                          </div>
+                          <div class="col-md-4">
+                            <div class="form-group">
+                              <label for="pwd">City:</label>
+                              <select disabled  class="form-control" id="city"  name="city">
+                                <?php if(isset($cities)){
+                                    foreach ($cities as  $city) { ?>
+                                <option value="<?php echo $city->id; ?>" <?php if($city->id == $customer_data->city_id){echo "selected";} ?>><?php echo $city->name; ?></option>
+                                <?php }} ?>
+                               
+                                
+                              </select>
+                            </div>
+
+                          </div>
+                        </div>
+
+                      <div class="form-group">
+                        <label for="pwd">Address:</label>
+                        <input type="text"  value="<?php echo $customer_data->address; ?>" readonly class="form-control" id="address"  name="address">
+                      </div>
+
+                       <div class="form-group">
+                        <label for="pwd">Address 2 (optional):</label>
+                        <input type="text" value="<?php echo $customer_data->address2; ?>" readonly class="form-control" id="address2"  name="address2">
+                      </div>
+
+                      <div class="form-group">
+                        <label for="pwd">Zip Code:</label>
+                        <input type="text" value="<?php echo $customer_data->zip_code; ?>" readonly class="form-control" id="zip_code"  name="zip_code">
+                      </div>
+                    <div class="form-group">
+                        <label for="pwd">Phone number:</label>
+                        <input type="text" value="<?php echo $customer_data->mobile; ?>" readonly class="form-control" id="phone"  name="phone">
+                      </div> 
+
+                    <div class="form-group">
+                        <label for="pwd">Security Question 1  :</label>
+                        <input type="text" value="<?php echo $customer_data->question1; ?>" readonly class="form-control" id="question1"  name="question1">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="pwd">Answer 1 :</label>
+                        <input type="text" value="<?php echo $customer_data->answer1; ?>" readonly class="form-control" id="answer1"  name="answer1">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="pwd">Security Question 2 :</label>
+                        <input type="text" value="<?php echo $customer_data->question2; ?>"  readonly class="form-control" id="question2"  name="question2">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="pwd">Answer 2 :</label>
+                        <input type="text" value="<?php echo $customer_data->answer2; ?>" readonly  class="form-control" id="answer2"  name="answer2">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="pwd">Security Question 3 :</label>
+                        <input type="text" value="<?php echo $customer_data->question3;?>" readonly class="form-control" id="question3"  name="question3">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="pwd">Answer 3 :</label>
+                        <input type="text" value="<?php echo $customer_data->answer3; ?>" readonly class="form-control" id="answer3"  name="answer3">
+                    </div>
+
+
+
+
+                    <div class="form-group">
+                      <label>Education</label>
+                        <div style="clear: both;"></div>
+
+                        <?php if($customer_data->education = 1){?>
+
+                      <div class="col-md-12">
+                        <div class="col-md-4">
+                              <label> <!-- <input type="checkbox" class="some check  name="education[]" value="GED"> --></label>
+                              GED
+                        </div>
+                          <div class="col-md-3"></div>
+                          <div class="col-md-5"></div>
+                    </div>
+                  <?php } elseif ($customer_data->education = 2) {?>
+                        
+                    <div class="col-md-12">
+                        <div class="col-md-4">
+                            <label> <input type="checkbox" class="some check  name="education[]" value="High School"></label>
+                            High School
+                        </div>
+                        <div class="col-md-3"></div>
+                        <div class="col-md-5"></div>
+                      </div>
+                    <?php } elseif ($customer_data->education = 3) {?>
+
+                      <div class="col-md-12">
+
+                        <div class="col-md-4">
+                            <label> <input type="checkbox" class="some check  name="education[]" value="College & Above"></label>
+                            College & Above
+                        </div>
+                        <div class="col-md-3"></div>
+                        <div class="col-md-5"></div>
+                       </div>
+                   </div>
+                 <?php } ?>
+
+                       <div style="clear: both;"></div>
+                  <br>
+                    
+                    
+                  
+
+                  <div class="form-group">
+                        <label for="pwd">Description :</label>
+                        <textarea type="text" class="form-control" id="description" readonly   name="description" rows="5"><?php echo $customer_data->description;?></textarea>
+                  </div>
+
+
+
+
+
+                  
+
+
+
+                  <div style="clear: both;"></div>
+                  <br>
+                      <div class="form-group">
+                        <label for="pwd">Status:</label>
+                        <select class="form-control" name="status" disabled>
+
+
+                          <option>
+                            
+                            <?php if($customer_data->status = 1 ){ ?>
+                                    Active
+                            <?php } elseif($customer_data->status = 0){?>
+
+                               Account Disable
+                            <?php } elseif($customer_data->status = 2){?>
+                                Pending for Approval
+                              <?php } ?>
+
+
+                          </option>
+                          <!-- <option value="0">Account Disable</option>
+                          <option value="1">Active</option>
+                          <option value="2">Pending for Approval</option> -->
+                        </select>
+                    </div>
+                  <?php }} ?>
+
+
+                    <!--  <input type="submit" name="add_provider" class="btn btn-default" value="Submit"> -->
+                 
+                    </form>
+                </div>
+                <!-- /tile body -->
+            </section>
+            </div>
+        <!-- /col -->
+    </div>
+    <!-- /row -->
+</div>
+<script type="text/javascript">
+
+    function someFunction(id) {
+        
+        var  textbox  = '.textbox'+id;
+        var checkboxe = '.check'+id;
+
+        if ($(checkboxe).is(":checked")) 
+          {
+          
+            $(textbox).css("display", "block");
+          
+          } else {
+          
+            $(textbox).css("display", "none");
+            $(textbox).val('');
+          
+          }
+    }
+</script>
+<script type="text/javascript">
+  $('.validate').keypress(function(event) {
+  if ((event.which != 46 || event.which === 8 || $(this).val().indexOf('.') != -1) && (event.which < 48 || event.which > 57 )  ) {
+    event.preventDefault();
+  }
+});
+</script>
+
+<script type="text/javascript">
+  $(document).ready(function(){
+
+    $('#country').on('change', function(){
+
+      var countryID = $(this).val();
+       var base_url_path = '<?php echo base_url();?>admin/getstate/'+countryID;
+       // alert(base_url_path);
+
+      if(countryID){
+
+        $.ajax({
+          type:'POST',
+          url : base_url_path,
+          data: 'country_id ='+countryID,
+          success: function(res){
+            console.log(res);
+              $('#city').empty();
+                $('#state').empty().append(res);
+
+            }
+        });
+      }
+  })
+
+
+
+    $('#state').on('change', function(){
+
+      var countryID = $(this).val();
+       var base_url_path = '<?php echo base_url();?>admin/getcity/'+countryID;
+       // alert(base_url_path);
+
+      if(countryID){
+
+        $.ajax({
+          type:'POST',
+          url : base_url_path,
+          data: 'country_id ='+countryID,
+          success: function(res){
+            console.log(res);
+                $('#city').empty().append(res);
+
+            }
+        });
+      }
+  })
+
+
+
+
+
+
+
+});
+
+
+</script>
+<script type="text/javascript">
+$('.clockpicker').clockpicker({
+    placement: 'top',
+    align: 'left',
+    donetext: 'Done'
+});
+</script>
